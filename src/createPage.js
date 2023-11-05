a6ccee7e67789e6c2528949c57ec6e69203201e6
Julia <jxlia.hx@gmail.com>
2023-11-05 14:44:41 -0600

    committing real quick to switch branches to get react-router setup in App.js and index.js

--- src/createPage.js
+++ src/createPage.js
@@ -1,1 +1,19 @@
 // this is my test page for commits
+import logo from './logo.svg';
+//import graphic from './sample.jpg'
+import graphic from './graphic.png'
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+
+function createPage() {
+    return(
+        <div className='TitleContainer'>
+            <div>
+                <header className='Title'>
+                    Create a Project
+                </header>
+            </div>
+        </div>
+    )
+}
