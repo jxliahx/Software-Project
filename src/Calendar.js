619783e34bf0fbfb3b5563e40d5c80aabfaaf321
Lauren-clarke <Lauren_raee@hotmail.com>
2023-11-08 18:57:35 -0600

    Added date pop up

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './Calendar.css';
 import NavigationBar from './NavigationBar';
+import TimeSchedule from './TimeSchedule'; // Import the TimeSchedule component
 
 function Calendar() {
   const [date, setDate] = useState(new Date());
   const [currentDate] = useState(new Date());
+  const [isTimeScheduleOpen, setTimeScheduleOpen] = useState(false);
 
+  const openTimeSchedule = () => {
+    setTimeScheduleOpen(true);
+  };
 
+  const closeTimeSchedule = () => {
+    setTimeScheduleOpen(false);
+  };
 
   return (
     <div className='app'>
@@ -17,13 +25,17 @@ function Calendar() {
         <h1 className='title'>Calendar</h1>
       </div>
       <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
+        <Calendar onChange={setDate} value={date} onClickDay={openTimeSchedule} />
       </div>
       <p className='date'>
         <span className='bold'>Selected Date:</span> {date.toDateString()}
         <br></br>
         <span className='bold'>Current Date:</span> {currentDate.toDateString()}
       </p>
+
+      {isTimeScheduleOpen && (
+        <TimeSchedule date={date} onClose={closeTimeSchedule} />
+      )}
     </div>
   );
 }
