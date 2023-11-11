e7193965d5709dc382127ef5b34595007cf27b43
Tung Ngo <logan78.ca@gmail.com>
2023-11-10 23:23:11 -0600

    correct cors origin

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const port = 5000;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
