8fd3041a147cf01b5ee132ba58f9b2a7d7f79d42
Tung Ngo <logan78.ca@gmail.com>
2023-11-11 20:23:01 -0600

    fixed misstyping

--- Backend/Routes/users.js
+++ Backend/Routes/users.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 router.use(cors());
 
-app.all("/", function (req, res, next) {
+router.all("/", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
