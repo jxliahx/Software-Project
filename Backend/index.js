4c4f6f45409badf43f9a279d5328c3515ab5f683
Tung Ngo <logan78.ca@gmail.com>
2023-11-12 22:58:04 -0600

    add new cors function

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
-app.use(
+/*app.use(
   cors({
     origin: [
       "https://studentgrouppm.onrender.com/",
@@ -18,12 +18,32 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
-);
+);*/
 
 //middleware
 app.use(express.json());
 //app.use(authJwt());
 //app.use(errorHandler());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://studentgrouppm.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 //Routes
 const usersRoutes = require("./Routes/users");
