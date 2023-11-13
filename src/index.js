c6015c13d7525acf1de5605acb1c766e569d9e97
kaitlynfarkas <123266446+kaitlynfarkas@users.noreply.github.com>
2023-11-13 12:17:00 -0600

    getting rid of firebase

--- src/index.js
+++ src/index.js
@@ -21,13 +21,14 @@ root.render(
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import Invitation from './Invitation';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Invitation/>
+    <App/>
   </React.StrictMode>
 );
 
